78594944db1029e933f608f1685e596b69a4c9c4
wmedrado <williamfranckm6@gmail.com>
2024-06-03 11:10:35 -0300

    Criação dos repositórios

---
+++ LanchesMac/Migrations/20240603130653_PopularLanches.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMac.Migrations
+{
+    public partial class PopularLanches : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Lanches() VALUES ()");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Lanches");
+        }
+    }
+}
---
+++ LanchesMac/Repositories/ILancheRepository.cs
@@ -0,0 +1,25 @@
+﻿using LanchesMac.Models;
+using LanchesMac.Models.Context;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Repositories
+{
+    public class LancheRepository : ILancheRepository
+    {
+        private readonly AppDbContext _context;
+        public LancheRepository(AppDbContext contexto) 
+        {
+            _context = contexto;
+        }
+        public IEnumerable<Lanche> Lanches => _context.Lanches.Include(c => c.Categoria);
+
+        public IEnumerable<Lanche> LanchesPreferidos => _context.Lanches.
+                                   Where(I => I.IsLanchePreferido).Include(c=> c.Categoria);
+
+        public Lanche GetLancheById(int lancheId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
