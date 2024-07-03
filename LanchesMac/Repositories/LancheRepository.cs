ff6d4f2abe1278558e60b1871dead8c48c66b078
William Medrado <williammedrado@pecege.com>
2024-07-03 18:16:34 -0300

    Criação da view List

--- LanchesMac/Repositories/LancheRepository.cs
+++ LanchesMac/Repositories/LancheRepository.cs
@@ -8,18 +8,19 @@ namespace LanchesMac.Repositories
     public class LancheRepository : ILancheRepository
     {
         private readonly AppDbContext _context;
-        public LancheRepository(AppDbContext contexto) 
+        public LancheRepository(AppDbContext contexto)
         {
             _context = contexto;
         }
         public IEnumerable<Lanche> Lanches => _context.Lanches.Include(c => c.Categoria);
 
-        public IEnumerable<Lanche> LanchesPreferidos => _context.Lanches.
-                                   Where(I => I.IsLanchePreferido).Include(c=> c.Categoria);
+        public IEnumerable<Lanche> LanchesPreferidos => _context.Lanches
+                                    .Where(I => I.IsLanchePreferido)
+                                    .Include(c => c.Categoria);
 
         public Lanche GetLancheById(int lancheId)
         {
-            throw new NotImplementedException();
+            return _context.Lanches.FirstOrDefault(l => l.LancheId == lancheId);
         }
     }
 }
